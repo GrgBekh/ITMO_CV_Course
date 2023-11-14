d925518cf75ebc5758e4b6c7b1b1ba0b072fdbba
TSheyd <sheydaevtimur@yandex.ru>
2023-11-14 13:25:37 +0300

    added model inference in web app

--- streamlit_map.py
+++ streamlit_map.py
@@ -4,6 +4,11 @@ from pathlib import Path
 import pydeck as pdk
 import tempfile
 
+import torch
+from torchvision import transforms
+from PIL import Image
+import cv2
+
 st.set_page_config(layout="wide")
 st.title("🔥 CV Model Demonstration App")
 
@@ -29,12 +34,86 @@ trajectory_data = [
 # Convert trajectory data to a DataFrame
 df_trajectory = pd.DataFrame(trajectory_data)
 
-# Function to process the video
+
+def load_model(model_path):
+    checkpoint = torch.load(model_path)
+    model = checkpoint['model']
+    model = model.float()  # Convert to full precision for inference on CPU
+    model.eval()
+    return model
+
+
+# model weights for inference
+model_path = "weights/35epochs.pt"  # Replace with the path to your model
+model = load_model(model_path)
+
+
+# Draws bounding boxes on a frame
+def draw_boxes(frame, boxes, color=(0, 255, 0), thickness=2):
+    for box in boxes:
+        x, y, w, h = box
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, thickness)
+    return frame
+
+
+def process_frame(frame):
+    # Preprocess frame if necessary
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(frame_rgb)
+    # Perform inference
+
+    # Define the transformation: convert the PIL image to a PyTorch tensor
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    # Apply the transformation to the image
+    image_tensor = transform(image).unsqueeze(0)  # Add batch dimension
+
+    # Perform inference
+    with torch.no_grad():
+        output = model(image_tensor)
+
+    processed_frame = draw_boxes(frame, output)
+    return processed_frame
+
+
+# Compiles a list of frames into a video file
+def compile_video(frames, output_path, frame_rate=30.0, frame_size=(1920, 1080), codec='mp4v'):
+    fourcc = cv2.VideoWriter_fourcc(*codec)
+    writer = cv2.VideoWriter(output_path, fourcc, frame_rate, frame_size)
+
+    for frame in frames:
+        writer.write(frame)
+
+    writer.release()
+
+
 def process_video(input_path):
-    # Your CV model processing code here
-    import time
-    time.sleep(5)  # Remove this in your actual function
-    return input_path
+    video = cv2.VideoCapture(input_path)
+    processed_frames = []
+
+    # Determine frame properties
+    frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_rate = video.get(cv2.CAP_PROP_FPS)
+
+    processed_frames = []
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+        processed_frame = process_frame(frame)
+        processed_frames.append(processed_frame)
+
+    # Compile processed_frames back into a video
+    # Path for the output video
+    output_video_path = f"{input_path}_processed.mp4"
+
+    # Compile the frames into a video
+    compile_video(processed_frames, output_video_path, frame_rate, (frame_width, frame_height))
+
+    return output_video_path
 
 
 # Function to create a map with points and text labels
@@ -107,11 +186,13 @@ if uploaded_file is not None and not st.session_state['video_processed']:
         tfile = tempfile.NamedTemporaryFile(
             delete=False, dir=temp_dir, suffix='.mp4')
         tfile.write(uploaded_file.read())
-        st.session_state['uploaded_video_path'] = tfile.name
+        # st.session_state['uploaded_video_path'] = tfile.name
         video_path = tfile.name
         tfile.close()
         
-        process_video(video_path)
+        output = process_video(video_path)
+        st.session_state['uploaded_video_path'] = output
+
         st.session_state['video_processed'] = True
         st.success('Extracting successful!')
 
